8f7dcebe0a2d1010380d5eadc8d57777857d5787
Kevin Chappell <kevin@chappell.dev>
2025-09-10 17:48:15 +0100

    update description block

--- blocks/description-block.php
+++ blocks/description-block.php
@@ -14,14 +14,18 @@ if (!defined('ABSPATH')) {
  */
 function kwik_ai_description_block_render($attributes)
 {
+  error_log('KWIK AI: kwik_ai_description_block_render() called'. print_r($attributes, true));
   $description = isset($attributes['description']) ? $attributes['description'] : '';
+  error_log('KWIK AI: Description content: ' . $description);
   
   if (empty($description)) {
     return '';
   }
 
+  // The description already contains HTML (paragraph tags from RichText.Content)
+  // so we don't need to wrap it in additional paragraph tags or escape it
   return sprintf(
-    '<div class="wp-block-kwik-ai-description"><p>%s</p></div>',
-    esc_html($description)
+    '<div class="wp-block-kwik-ai-description">%s</div>',
+    $description
   );
 }
