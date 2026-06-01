0df93ab337d6b64ad27674a7c0bc27e27320dd3d
Kevin Chappell <kevin@chappell.dev>
2026-06-01 08:25:22 +0100

    fix: description controls style, test time constants

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,6 +20,26 @@ if (!is_dir(ABSPATH)) {
     mkdir(ABSPATH, 0755, true);
 }
 
+// WordPress time constants
+if (!defined('MINUTE_IN_SECONDS')) {
+    define('MINUTE_IN_SECONDS', 60);
+}
+if (!defined('HOUR_IN_SECONDS')) {
+    define('HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS);
+}
+if (!defined('DAY_IN_SECONDS')) {
+    define('DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS);
+}
+if (!defined('WEEK_IN_SECONDS')) {
+    define('WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS);
+}
+if (!defined('MONTH_IN_SECONDS')) {
+    define('MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS);
+}
+if (!defined('YEAR_IN_SECONDS')) {
+    define('YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS);
+}
+
 // Mock WordPress functions and classes
 if (!function_exists('get_option')) {
     function get_option($option, $default = false) {
